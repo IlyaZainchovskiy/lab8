4c1f19742763399f87c62e6469399a2490a7b0cc
IlyaZainchovskiy <zainchkovskiy1@gmail.com>
2026-05-17 19:38:02 +0300

    .

---
+++ CookbookData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace lab8
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public List<Recipe> Recipes { get; set; } = new List<Recipe>();
+    }
+
+    public class Recipe
+    {
+        public int RecipeId { get; set; }
+        public string Title { get; set; }
+        public string Instructions { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+
+    public class CookbookDbContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Recipe> Recipes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source=cookbook_mvvm.db");
+        }
+    }
+}
---
+++ MainViewModel.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace lab8
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private readonly CookbookDbContext _db;
+
+        public ObservableCollection<Recipe> Recipes { get; set; }
+
+        private string _newRecipeTitle;
+        public string NewRecipeTitle
+        {
+            get => _newRecipeTitle;
+            set { _newRecipeTitle = value; OnPropertyChanged(); AddCommand.RaiseCanExecuteChanged(); }
+        }
+
+        private string _newRecipeInstructions;
+        public string NewRecipeInstructions
+        {
+            get => _newRecipeInstructions;
+            set { _newRecipeInstructions = value; OnPropertyChanged(); }
+        }
+
+        public int TotalCount => Recipes?.Count ?? 0;
+
+        public RelayCommand AddCommand { get; }
+        public RelayCommand SortCommand { get; }
+
+        public MainViewModel()
+        {
+            _db = new CookbookDbContext();
+            _db.Database.EnsureCreated();
+
+            if (!_db.Categories.Any())
+            {
+                _db.Categories.Add(new Category { Name = "Основні страви" });
+                _db.SaveChanges();
+            }
+
+            Recipes = new ObservableCollection<Recipe>();
+            LoadData();
+
+            AddCommand = new RelayCommand(AddRecipe, CanAddRecipe);
+            SortCommand = new RelayCommand(SortRecipes);
+        }
+
+        private void LoadData()
+        {
+            var dbRecipes = _db.Recipes.Include(r => r.Category).ToList();
+            Recipes.Clear();
+            foreach (var r in dbRecipes)
+            {
+                Recipes.Add(r);
+            }
+            OnPropertyChanged(nameof(TotalCount)); 
+        }
+
+        private bool CanAddRecipe()
+        {
+            return !string.IsNullOrWhiteSpace(NewRecipeTitle);
+        }
+
+        private void AddRecipe()
+        {
+            var category = _db.Categories.FirstOrDefault();
+            var newRecipe = new Recipe
+            {
+                Title = NewRecipeTitle,
+                Instructions = NewRecipeInstructions,
+                CategoryId = category?.CategoryId ?? 1
+            };
+
+            _db.Recipes.Add(newRecipe);
+            _db.SaveChanges();
+
+            Recipes.Add(newRecipe);
+            OnPropertyChanged(nameof(TotalCount)); 
+
+            NewRecipeTitle = string.Empty;
+            NewRecipeInstructions = string.Empty;
+        }
+
+        private void SortRecipes()
+        {
+            var sorted = Recipes.OrderBy(r => r.Title).ToList();
+            Recipes.Clear();
+            foreach (var r in sorted)
+            {
+                Recipes.Add(r);
+            }
+        }
+    }
+}
